de9bf2ff85c29483322588045f2c5d78b9ee66a3
kavi17498 <kavindulakshan187@gmail.com>
2024-04-09 23:10:36 -0700

    changed

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -1,3 +1,4 @@
+// GoogleMap.js
 import React from 'react';
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 
@@ -9,19 +10,18 @@ const containerStyle = {
 function Map() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: 'AIzaSyCCT66DwDNlwEDgJxhKcaFpdQx9DQ6EDI0',
+    googleMapsApiKey: 'YOUR_API_KEY',
   });
 
-  const [map, setMap] = React.useState(null);
+  const [map] = React.useState(null); // Remove unused variable 'map'
 
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds();
     map.fitBounds(bounds);
-    setMap(map);
   }, []);
 
   const onUnmount = React.useCallback(function callback(map) {
-    setMap(null);
+    // Handle cleanup if needed
   }, []);
 
   return isLoaded ? (
@@ -32,10 +32,8 @@ function Map() {
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
-      { /* Child components, such as markers, go here */ }
-      <Marker
-        position={{ lat: -3.745, lng: -38.523 }}
-      />
+      {/* Child components, such as markers, go here */}
+      <Marker position={{ lat: -3.745, lng: -38.523 }} />
     </GoogleMap>
   ) : <></>;
 }
